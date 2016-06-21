a18b1fbd7853b7131d69b43c030b7610b28a55fc
Brendan Vavra <brendan.vavra@gmail.com>
2016-06-21 08:53:55 -0400

    Renamed tutorial from 4 to 2 (to match sequence instead of count, which could be variable).  Also cleaned up the tutorial.

--- Prototypes/Opto_Interruptor/Opto_Interruptor_2/Opto_Interruptor_2.ino
+++ Prototypes/Opto_Interruptor/Opto_Interruptor_2/Opto_Interruptor_2.ino
@@ -1,11 +1,11 @@
 /* 
-* 4 Interruptors - suggest trying 2x2, 1x4, and 4x1
-* At this point most of this code is pulled directly from AccordionMega's code.
+* Matrix of Interruptors - it's recommended to try 2x2, 1x4, and 4x1, but this code supports up to 8x4 as written.
+* At this point much of this code is pulled directly from AccordionMega's code.
 */
 
+//This prototype currently supports 4 digital pins, 
+//but you can add as many as you want by extending the two arrays below:
 char DIGITAL_COLUMNS[] = { 9, 10, 11, 12 };
-
-//Note: in this example we're only using two bits from each byte
 int KeysStatus[] = {  
   B0000000,
   B0000000,
@@ -13,7 +13,7 @@ int KeysStatus[] = {
   B0000000
 };
 
-//this is the value of all pins from the analog port (7 and 6 don't exist on UNO)
+//this is the value of all pins from the analog port (Note: analog pins 7 and 6 don't exist on UNO)
 //B00000000
 //B76543210
 byte reg_values = 0;
@@ -41,9 +41,9 @@ void setup()
 void loop()
 {
   for (int i=0; i<sizeof(DIGITAL_COLUMNS);i++){ 
-    //I wonder if we can replace this with direct port write for speed?
+    //TODO: I wonder if we can replace this with a direct port write for better performance?
     digitalWrite(DIGITAL_COLUMNS[i], HIGH);
-    delayMicroseconds(500);//TODO - play around with this number a bit
+    delayMicroseconds(500);//This delay value has proven to work with the ITR9608
     
     #if defined (__AVR_ATmega328P__)
       reg_values = ~PINC;
@@ -54,16 +54,17 @@ void loop()
     digitalWrite(DIGITAL_COLUMNS[i], LOW);
 
     if (reg_values != KeysStatus[i]){
-      if (reg_values > KeysStatus[i]){             
-        check_key(reg_values ^ KeysStatus[i],i,true);  //sending modified bits only
+      if (reg_values > KeysStatus[i]){ //if it's greater, we're turning the note on; else, turning it off.
+        check_key(reg_values ^ KeysStatus[i],i,true);  //using bit-wise OR to send modified bits only
       }
       else {
-        check_key(reg_values ^ KeysStatus[i],i,false); //sending modified bits only
+        check_key(reg_values ^ KeysStatus[i],i,false); //using bit-wise OR to send modified bits only
       }           
     }
   }
 }
 
+//Instead of iterating the array from 0-7, use a binary search to find the modified bits faster
 void check_key(int reg, int group, boolean up){
    // saving 4 iterations, dividing byte by 2
    if (reg & 0xF0) {
@@ -82,9 +83,10 @@ void check_key(int reg, int group, boolean up){
    }
 }
 
-//The right two most bits turn on because those pins don't exist on the Uno
+//This is the mapping of pitches on the right hand of the accordion.
+//Note: The right two most bits will always turn on for the UNO because those pins don't exist.
+//You can stop this from happening by commenting out the right-most two values in the top row.
 const char right_notes_midi_numbers[][8] = {
-  {53,55,57,59,60,62,64,65},//Added for test setup
   {60,59,58,57,56,55,54,53},
   {68,67,66,65,64,63,62,61},
   {76,75,74,73,72,71,70,69},
@@ -93,13 +95,14 @@ const char right_notes_midi_numbers[][8] = {
   {0,0,0,0,0,0,0,93}
 };
 
-byte midi_channel1 = 0;
-byte midi_channel2 = 1;
 char notes_to_play;
 
+//Set the state of the notes pressed and print data for the new note
 void note_midi(int group, int position, boolean on){
+  
   int pitch;
   String str_warn;
+  byte midi_channel = 1;
   char midi_cmd;
   char curr_register = 0;
   int midi_vel;
@@ -109,7 +112,7 @@ void note_midi(int group, int position, boolean on){
 
   if(on) {
     str_warn = "Note on ";
-    midi_cmd = midi_channel2 | 0x90;
+    midi_cmd = midi_channel | 0x90;
     KeysStatus[group] |= (1 << position);  //setting bit value
     Serial.print(" Changed Status: ");
     Serial.println(KeysStatus[group], BIN);
@@ -117,7 +120,7 @@ void note_midi(int group, int position, boolean on){
   }
   else if(~on) {
     str_warn = "Note off ";
-    midi_cmd = midi_channel2 | 0x80;
+    midi_cmd = midi_channel | 0x80;
     KeysStatus[group] &= ~(1 << position);  //setting bit value
     Serial.print(" Changed Status: ");
     Serial.println(KeysStatus[group], BIN);
@@ -135,7 +138,9 @@ void note_midi(int group, int position, boolean on){
   
   notes_to_play = right_notes_midi_numbers[group][position];
   if (notes_to_play){
-    //TODO - replace with ArduinoMIDI library command
+    //This is where we'd print the MIDI Note On/Off command with the ArduinoMIDI library
+    //See the Opto-Interruptor MIDI prototype or the final project for more info.
+    
     //Serial1.print(midi_cmd, BYTE);
     //Serial1.print(notes_to_play, BYTE);
     //Serial1.print(midi_vel, BYTE);
