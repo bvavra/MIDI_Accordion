d7324588196137c16c0a47ca4012dce8577aed84
Brendan Vavra <brendan.vavra@gmail.com>
2017-01-09 20:53:23 -0500

    Leaving BMP commented out by default

--- MIDI_Accordion/MIDI_Accordion.ino
+++ MIDI_Accordion/MIDI_Accordion.ino
@@ -25,7 +25,7 @@ struct MySettings : public midi::DefaultSettings
 
 //#define DEBUG//uncomment this line to print serial messages, comment to send MIDI data
 //#define BLUETOOTH//uncomment this line to send MIDI data via bluetooth instead of USB
-#define BMP//uncomment this line to use the BMP180 to add dynamics via bellows
+//#define BMP//uncomment this line to use the BMP180 to add dynamics via bellows
 
 #ifdef BLUETOOTH
   MIDI_CREATE_CUSTOM_INSTANCE(HardwareSerial, Serial1, MIDI, MySettings);
