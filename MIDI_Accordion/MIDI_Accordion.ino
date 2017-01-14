390d7121ef12d0ad9b1ae561583c6910ae6ac5cc
Brendan Vavra <brendan.vavra@gmail.com>
2017-01-14 08:39:20 -0500

    Added GPL license text and disclaimer

--- MIDI_Accordion/MIDI_Accordion.ino
+++ MIDI_Accordion/MIDI_Accordion.ino
@@ -1,8 +1,23 @@
-/* 
-* MIDI Accordion
-* Author: Brendan Vavra 
-* Thanks to Dimon Yegorenkov for the original code of which much of this project is based.
-*/
+/*******************************************************************************
+ MIDI Accordion
+ https://github.com/bvavra/MIDI_Accordion
+ Copyright © 2016-2017 Brendan Vavra
+ based on projects by Dimon Yegorenkov 2011 and Jason Bugeja 2014
+ *******************************************************************************
+ This program is free software: you can redistribute it and/or modify
+ it under the terms of the GNU General Public License as published by
+ the Free Software Foundation, either version 3 of the License, or
+ (at your option) any later version.
+   
+ This program is distributed in the hope that it will be useful,
+ but WITHOUT ANY WARRANTY; without even the implied warranty of
+ MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ GNU General Public License for more details.
+
+ You should have received a copy of the GNU General Public License
+ along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ *******************************************************************************/
+
 #include <MIDI.h>
 #include <midi_Defs.h>
 #include <midi_Message.h>
